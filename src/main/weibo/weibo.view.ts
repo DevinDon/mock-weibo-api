81f1d6e99dfd81bfcf62a15232063051c30fb8d1
Devin Don <I.INF@Outlook.com>
2020-07-26 11:57:58 +0800

    feat(weibo): add index description

--- src/main/weibo/weibo.view.ts
+++ src/main/weibo/weibo.view.ts
@@ -11,6 +11,16 @@ export class WeiboView {
   @Inject()
   private controller!: WeiboController;
 
+  @GET()
+  async index() {
+    return {
+      可用功能: '请求方式 对应链接',
+      getCode: 'GET oauth2/authorize',
+      getToken: 'POST oauth2/access_token',
+      getPublicTimeline: 'GET 2/statuses/public_timeline.json'
+    };
+  }
+
   @GET('oauth2/authorize')
   async getCode() {
     return { code: CODE };
