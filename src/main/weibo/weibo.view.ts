52a4dc9fb2344c28e883d18b1f42b0f951557ffb
Devin Don <I.INF@Outlook.com>
2020-07-26 18:46:05 +0800

    feat(weibo): add index view

--- src/main/weibo/weibo.view.ts
+++ src/main/weibo/weibo.view.ts
@@ -1,4 +1,6 @@
 import { GET, Handler, Inject, PathQuery, POST, View } from '@rester/core';
+import { readFileSync } from 'fs';
+import { HTMLHandler } from '../handler/html';
 import { RedirectToCallback } from '../handler/redirect';
 import { CODE, TOKEN } from './data';
 import { WeiboController } from './weibo.controller';
@@ -6,20 +8,18 @@ import { WeiboController } from './weibo.controller';
 // add, remove, modify, find(condition), get(random)
 // one, more
 
+const INDEX = readFileSync('src/main/weibo/index.html');
+
 @View('weibo')
 export class WeiboView {
 
   @Inject()
   private controller!: WeiboController;
 
+  @Handler(HTMLHandler)
   @GET()
   async index() {
-    return {
-      可用功能: '请求方式 对应链接',
-      getCode: 'GET oauth2/authorize',
-      getToken: 'POST oauth2/access_token',
-      getPublicTimeline: 'GET 2/statuses/public_timeline.json'
-    };
+    return INDEX;
   }
 
   @Handler(RedirectToCallback)
