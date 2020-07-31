d9c6c8b19c76f4ba098d61ebf2279f4c80f681f1
Devin Don <I.INF@Outlook.com>
2020-07-31 17:59:36 +0000

    feat(util): add one by one insert

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,3 +1,18 @@
 export function randomSort() {
   return Math.random() - 0.5;
 }
+
+export async function insertOneByOne<T>(data: T[], fn: (...args: any) => Promise<any>) {
+  const results = await Promise.all(
+    data.map(
+      item => fn(item)
+        .then(() => ({ id: item['id'] }))
+        .catch(() => ({ id: item['id'], failed: true }))
+    )
+  );
+  return {
+    total: results.length,
+    failed: results.filter(result => result['failed']).length,
+    results
+  };
+}
