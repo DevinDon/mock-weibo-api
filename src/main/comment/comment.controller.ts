ad21cadee96ee74c47fd791764b591a360eca465
Devin Don <I.INF@Outlook.com>
2020-08-07 17:38:06 +0000

    feat(comment): allow to get comments

--- src/main/comment/comment.controller.ts
+++ src/main/comment/comment.controller.ts
@@ -1,17 +1,40 @@
-import { logger } from '@iinfinity/logger';
 import { Controller } from '@rester/core';
-import { get } from 'superagent';
 import { getMongoRepository } from 'typeorm';
+import { SHOW_COMMENTS } from '../@constant';
 import { StatusEntity } from '../status/status.entity';
-import { Status } from '../status/status.model';
-import { insertOneByOne } from '../@util';
 import { CommentEntity } from './comment.entity';
 import { Comment } from './comment.model';
 
 // insert, delete, update, select
 // one, more
 
+export interface SelectCommentsParam {
+  id: number;
+  skip: number;
+  take: number;
+}
+
 @Controller()
 export class CommentController {
+
+  async selectCommentsByStatusID({ id, skip, take: limit }: SelectCommentsParam) {
+    // 返回最新的评论
+    const cursor = getMongoRepository(CommentEntity)
+      .createCursor({ 'status.id': id })
+      .sort({ $natural: -1 })
+      .skip(skip)
+      .limit(limit);
+    const comments: Comment[] = [];
+    for await (const status of cursor) {
+      comments.push(status);
+    }
+    const result = {
+      ...SHOW_COMMENTS,
+      comments,
+      status: await StatusEntity.findOne({ id })
+    };
+    result.total_number = result.comments.length;
+    return result;
+  }
 
 }
--- src/main/comment/comment.view.ts
+++ src/main/comment/comment.view.ts
@@ -1,13 +1,31 @@
-import { HTTP400Exception, Inject, PathVariable, PUT, View } from '@rester/core';
-import { CommentController } from './comment.controller';
+import { GET, HTTP400Exception, Inject, PathQuery, View } from '@rester/core';
+import { CommentController, SelectCommentsParam } from './comment.controller';
 
 // add, remove, modify, find(condition), get(random)
 // one, more
 
-@View('weibo/0/comment')
+@View('weibo/2/comments')
 export class CommentView {
 
   @Inject()
   private controller!: CommentController;
+
+  @GET('show.json')
+  async showCommentsByStatusID(
+    @PathQuery('id') id: number,
+    @PathQuery('count') count: number = 20,
+    @PathQuery('page') page: number = 1
+  ) {
+    if (!id) { throw new HTTP400Exception('param id is required'); }
+    id = +id;
+    count = +count || 20;
+    page = +page || 1;
+    const param: SelectCommentsParam = {
+      id,
+      skip: Math.max(0, page - 1) * count,
+      take: count
+    };
+    return this.controller.selectCommentsByStatusID(param);
+  }
 
 }
