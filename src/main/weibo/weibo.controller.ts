220860acbed2b672ff9e36f25ba612b961539d31
Devin Don <I.INF@Outlook.com>
2020-08-07 17:26:00 +0000

    perf(weibo): remove useless code

--- src/main/weibo/weibo.controller.ts
+++ src/main/weibo/weibo.controller.ts
@@ -1,30 +1,9 @@
 import { Controller } from '@rester/core';
-import { getHomeTimeline, HomeTimelineParam } from './data/home-timeline';
-import { getPublicTimeline, PublicTimelineParam } from './data/public-timeline';
-import { showComments, ShowCommentsParam } from './data/show-comments';
-import { WeiboEntity } from './weibo.entity';
-import { Weibo } from './weibo.model';
 
 // insert, delete, update, select
 // one, more
 
 @Controller()
 export class WeiboController {
-
-  async selectOneByID(id: Weibo['id']) {
-    return WeiboEntity.findOne(id);
-  }
-
-  async showComments(param: ShowCommentsParam) {
-    return showComments(param);
-  }
-
-  async getPublicTimeline(param: PublicTimelineParam) {
-    return getPublicTimeline(param);
-  }
-
-  async getHomeTimeline(param: HomeTimelineParam) {
-    return getHomeTimeline(param);
-  }
 
 }
--- src/main/weibo/weibo.view.ts
+++ src/main/weibo/weibo.view.ts
@@ -2,13 +2,11 @@ import { GET, Handler, HTTP400Exception, Inject, PathQuery, POST, View } from '@
 import { readFileSync } from 'fs';
 import { HTMLHandler } from '../@handler/html.handler';
 import { RedirectToCallback } from '../@handler/redirect.handler';
-import { CODE, TOKEN } from './data';
+import { CODE, TOKEN } from '../@constant';
 import { WeiboController } from './weibo.controller';
 
 // add, remove, modify, find(condition), get(random)
 // one, more
-
-const INDEX = readFileSync('src/main/@public/index.html');
 
 @View('weibo')
 export class WeiboView {
@@ -16,10 +14,16 @@ export class WeiboView {
   @Inject()
   private controller!: WeiboController;
 
+  private readonly HTML: any = {};
+
+  constructor() {
+    this.HTML.index = readFileSync('src/main/@public/index.html');
+  }
+
   @Handler(HTMLHandler)
   @GET()
   async index() {
-    return INDEX;
+    return this.HTML.index;
   }
 
   @Handler(RedirectToCallback)
@@ -43,37 +47,17 @@ export class WeiboView {
     return TOKEN;
   }
 
-  @GET('2/statuses/public_timeline.json')
-  async getPublicTimeline(
-    @PathQuery('count') count: number = 20,
-    @PathQuery('page') page: number = 1
-  ) {
-    count = Math.min(50, +count);
-    page = +page || 1;
-    return this.controller.getPublicTimeline({ count, page });
-  }
-
-  @GET('2/statuses/home_timeline.json')
-  async getHomeTimeline(
-    @PathQuery('count') count: number = 20,
-    @PathQuery('page') page: number = 1
-  ) {
-    count = Math.min(20, +count);
-    page = +page || 1;
-    return this.controller.getHomeTimeline({ count, page });
-  }
-
-  @GET('2/comments/show.json')
-  async showCommentsByStatusID(
-    @PathQuery('id') id: number,
-    @PathQuery('count') count: number = 20,
-    @PathQuery('page') page: number = 1
-  ) {
-    if (!id) { throw new HTTP400Exception('param id is required'); }
-    id = +id;
-    count = +count || 20;
-    page = +page || 1;
-    return this.controller.showComments({ id, count, page });
-  }
+  // @GET('2/comments/show.json')
+  // async showCommentsByStatusID(
+  //   @PathQuery('id') id: number,
+  //   @PathQuery('count') count: number = 20,
+  //   @PathQuery('page') page: number = 1
+  // ) {
+  //   if (!id) { throw new HTTP400Exception('param id is required'); }
+  //   id = +id;
+  //   count = +count || 20;
+  //   page = +page || 1;
+  //   return this.controller.showComments({ id, count, page });
+  // }
 
 }
