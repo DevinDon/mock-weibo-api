449a88d34af9cc7ab40aab41d2efdcaf8ed35fcf
Devin Don <I.INF@Outlook.com>
2020-08-08 14:41:26 +0000

    feat(comment): add create, reply & destory apis

--- src/main/comment/comment.controller.ts
+++ src/main/comment/comment.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@rester/core';
 import { getMongoRepository } from 'typeorm';
 import { SHOW_COMMENTS } from '../@constant';
 import { StatusEntity } from '../status/status.entity';
+import { User } from '../user/user.model';
 import { CommentEntity } from './comment.entity';
 import { Comment } from './comment.model';
 
@@ -12,6 +13,24 @@ export interface SelectCommentsParam {
   id: number;
   skip: number;
   take: number;
+}
+
+export interface InsertCommentForStatusParam {
+  id: number;
+  comment: string;
+  user: User;
+}
+
+export interface InsertCommentForCommentParam {
+  id: number;
+  cid: number;
+  comment: string;
+  user: User;
+}
+
+export interface DeleteCommentParam {
+  cid: number;
+  user: User;
 }
 
 @Controller()
@@ -35,6 +54,53 @@ export class CommentController {
     };
     result.total_number = result.comments.length;
     return result;
+  }
+
+  async insertCommentByStatusID({ id, comment, user }: InsertCommentForStatusParam) {
+    const status = await StatusEntity.findOne({ id });
+    if (!status) { return { status: `status ${id} is not exist` }; }
+    const newComment: Comment = {
+      id: Date.now(),
+      created_at: new Date().toString(),
+      text: comment,
+      user,
+      status: status as any
+    } as any;
+    const result = await CommentEntity.insert(newComment);
+    return newComment;
+  }
+
+  async insertCommentByCommentID({ id, cid, comment, user }: InsertCommentForCommentParam) {
+    const status = await StatusEntity.findOne({ id });
+    if (!status) { return { status: `status ${id} is not exist` }; }
+    const replyComment = await CommentEntity.findOne(
+      { id: cid },
+      { select: ['_id', 'id', 'content', 'created_at', 'text', 'user'] }
+    );
+    if (!replyComment) { return { status: `comment ${cid} is not exist` }; }
+    const newComment: Comment = {
+      id: +`${Date.now()}${Math.random().toString().slice(2, 6)}`,
+      created_at: new Date().toString(),
+      text: comment,
+      reply_comment: replyComment,
+      user,
+      status: status as any
+    } as any;
+    const result = await CommentEntity.insert(newComment);
+    return newComment;
+  }
+
+  async deleteCommentByCommentID({ cid, user }: DeleteCommentParam) {
+    const comment = await CommentEntity.findOne({
+      where: {
+        id: cid,
+        'user.id': user.id
+      }
+    });
+    if (!comment) {
+      return { status: `comment ${cid} is not exist` };
+    }
+    return comment.remove();
   }
 
 }
--- src/main/comment/comment.view.ts
+++ src/main/comment/comment.view.ts
@@ -1,4 +1,6 @@
-import { GET, HTTP400Exception, Inject, PathQuery, POST, View } from '@rester/core';
+import { GET, Handler, HandlerZone, HTTP400Exception, Inject, PathQuery, POST, View } from '@rester/core';
+import { AuthHandler } from '../@handler/auth.handler';
+import { User } from '../user/user.model';
 import { CommentController, SelectCommentsParam } from './comment.controller';
 
 // add, remove, modify, find(condition), get(random)
@@ -28,21 +30,25 @@ export class CommentView {
     return this.controller.selectCommentsByStatusID(param);
   }
 
+  @Handler(AuthHandler)
   @POST('create.json')
   async create(
     @PathQuery('id') id: number,
-    @PathQuery('comment') comment: string
+    @PathQuery('comment') comment: string,
+    @HandlerZone() { user }: { user: User }
   ) {
     if (!id) { throw new HTTP400Exception('param id is required'); }
     if (!comment) { throw new HTTP400Exception('param id is required'); }
-    return this.controller.insertCommentByStatusID({ id: +id, comment });
+    return this.controller.insertCommentByStatusID({ id: +id, comment, user });
   }
 
+  @Handler(AuthHandler)
   @POST('reply.json')
   async reply(
     @PathQuery('id') id: number,
     @PathQuery('cid') cid: number,
-    @PathQuery('comment') comment: string
+    @PathQuery('comment') comment: string,
+    @HandlerZone() { user }: { user: User }
   ) {
     if (!id) { throw new HTTP400Exception('param id is required'); }
     if (!cid) { throw new HTTP400Exception('param cid is required'); }
@@ -50,16 +56,19 @@ export class CommentView {
     return this.controller.insertCommentByCommentID({
       id: +id,
       cid: +cid,
-      comment: encodeURIComponent(comment)
+      comment: encodeURIComponent(comment),
+      user
     });
   }
 
+  @Handler(AuthHandler)
   @POST('destory.json')
   async destory(
-    @PathQuery('cid') cid: number
+    @PathQuery('cid') cid: number,
+    @HandlerZone() { user }: { user: User }
   ) {
     if (!cid) { throw new HTTP400Exception('param cid is required'); }
-    return this.controller.deleteCommentByCommentID(+cid);
+    return this.controller.deleteCommentByCommentID({ cid: +cid, user });
   }
 
 }
