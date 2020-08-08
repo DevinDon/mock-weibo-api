f3b74cf0575704b69eb03e0e36a5770281b1d160
Devin Don <I.INF@Outlook.com>
2020-08-08 02:49:01 +0000

    feat(comment): add path view

--- src/main/comment/comment.view.ts
+++ src/main/comment/comment.view.ts
@@ -1,4 +1,4 @@
-import { GET, HTTP400Exception, Inject, PathQuery, View } from '@rester/core';
+import { GET, HTTP400Exception, Inject, PathQuery, POST, View } from '@rester/core';
 import { CommentController, SelectCommentsParam } from './comment.controller';
 
 // add, remove, modify, find(condition), get(random)
@@ -26,6 +26,40 @@ export class CommentView {
       take: count
     };
     return this.controller.selectCommentsByStatusID(param);
+  }
+
+  @POST('create.json')
+  async create(
+    @PathQuery('id') id: number,
+    @PathQuery('comment') comment: string
+  ) {
+    if (!id) { throw new HTTP400Exception('param id is required'); }
+    if (!comment) { throw new HTTP400Exception('param id is required'); }
+    return this.controller.insertCommentByStatusID({ id: +id, comment });
+  }
+
+  @POST('reply.json')
+  async reply(
+    @PathQuery('id') id: number,
+    @PathQuery('cid') cid: number,
+    @PathQuery('comment') comment: string
+  ) {
+    if (!id) { throw new HTTP400Exception('param id is required'); }
+    if (!cid) { throw new HTTP400Exception('param cid is required'); }
+    if (!comment) { throw new HTTP400Exception('param id is required'); }
+    return this.controller.insertCommentByCommentID({
+      id: +id,
+      cid: +cid,
+      comment: encodeURIComponent(comment)
+    });
+  }
+
+  @POST('destory.json')
+  async destory(
+    @PathQuery('cid') cid: number
+  ) {
+    if (!cid) { throw new HTTP400Exception('param cid is required'); }
+    return this.controller.deleteCommentByCommentID(+cid);
   }
 
 }
