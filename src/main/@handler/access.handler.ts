d4fc575d9398c0fe36a513a169b7356b576aa85b
Devin Don <I.INF@Outlook.com>
2020-08-18 10:18:00 +0800

    fix(@handler/access): fix url format

--- src/main/@handler/access.handler.ts
+++ src/main/@handler/access.handler.ts
@@ -9,7 +9,7 @@ export class AccessHandler extends BaseHandler {
   async handle(next: () => Promise<any>): Promise<any> {
 
     const result = await next();
-    const url = new URL(this.request.url || 'http://wrong');
+    const url = new URL('http://mock.don.red' + this.request.url || 'http://wrong');
     AccessEntity.insert({
       date: new Date(),
       address: JSON.stringify(this.request.headers['x-real-ip']) || this.request.connection.remoteAddress || '',
