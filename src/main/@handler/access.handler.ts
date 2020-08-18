93862b7a1ff0d1355f1d26c9e8973f25efc619b3
Devin Don <I.INF@Outlook.com>
2020-08-18 10:15:12 +0800

    fix(@handler/access): fix query

--- src/main/@handler/access.handler.ts
+++ src/main/@handler/access.handler.ts
@@ -9,20 +9,13 @@ export class AccessHandler extends BaseHandler {
   async handle(next: () => Promise<any>): Promise<any> {
 
     const result = await next();
-    // .catch(async error => {
-    //   logger.warn(typeof error, JSON.stringify(error), error.code === 11600, error.name === 'MongoError');
-    //   if (error.name === 'MongoError' || error.code === 11600) {
-    //     logger.error(`Database down: ${error}`);
-    //     await this.rester.connectDatabase(1000).catch(reason => logger.warn(reason));
-    //   }
-    //   throw error;
-    // });
+    const url = new URL(this.request.url || 'http://wrong');
     AccessEntity.insert({
       date: new Date(),
       address: JSON.stringify(this.request.headers['x-real-ip']) || this.request.connection.remoteAddress || '',
       url: this.request.url,
-      path: this.route.mapping.path,
-      query: this.route.mapping.queryObject,
+      path: url.pathname,
+      query: Object.fromEntries(url.searchParams.entries()),
       headers: this.request.headers as any || [],
       statusCode: this.response.statusCode,
       statusMessage: this.response.statusMessage
