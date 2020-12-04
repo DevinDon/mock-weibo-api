4c4d021c9cbcb2ce6b352b0eb667fbef7571cc31
Devin Don <I.INF@Outlook.com>
2020-12-04 18:56:11 +0800

    fix(push): fix docker file

--- script/push.sh
+++ script/push.sh
@@ -5,8 +5,15 @@ then
 fi
 cd dist
 tee > ./Dockerfile <<-'EOF'
-FROM iinfinity/nginx
-COPY ["./", "/app/"]
+FROM iinfinity/node
+
+COPY dist/index.js /app/index.js
+COPY src/main/@public /app/@public
+COPY rester.json /app/rester.json
+WORKDIR /app
+ENV MODE=PROD
+
+ENTRYPOINT [ "node", "index.js" ]
 EOF
 docker build -t $registry/$npm_package_name .
 docker push $registry/$npm_package_name
